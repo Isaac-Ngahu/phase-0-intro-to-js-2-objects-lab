84aa916188ca3d633a97e6710171412af42d261b
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2022-11-23 01:45:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee={
+    name:"Lover",
+    streetAddress:"69",
+}
+function updateEmployeeWithKeyAndValue(_employeeObject,key,value){
+    const newObject={...employee};
+    newObject[key]=value;
+    return newObject;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject,key,value){
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(_employee, key){
+   const newObject={...employee};
+    delete newObject[key];
+    return newObject;
+
+}
+function destructivelyDeleteFromEmployeeByKey(_employee,key){
+    delete employee[key];
+    return employee;
+
+}
